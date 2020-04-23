d9c9e436c924a6d899933689e57b0d4794675a6e
PalakSDarji <palaksdarji@gmail.com>
2020-04-23 19:42:17 +0530

    second activity is attached to nav_graph

--- app/src/main/java/com/palak/mynavdemo/ViewTransactionFragment.kt
+++ app/src/main/java/com/palak/mynavdemo/ViewTransactionFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_view_transaction.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class ViewTransactionFragment : Fragment() {
+class ViewTransactionFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +22,19 @@ class ViewTransactionFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_view_transaction, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+        btnSecActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.btnSecActivity -> navController.navigate(R.id.action_viewTransactionFragment_to_seeProfileActivity)
+        }
     }
 
 }
